14c1b30ecd5cc8395b4069414ab51d5b3ceafac1
Nick <hello@swiftful-thinking.com>
2025-10-18 17:46:59 -0400

    [Bug] Fix clipping count

--- Sources/SwiftfulLoggingFirebaseAnalytics/Extensions/String+EXT.swift
+++ Sources/SwiftfulLoggingFirebaseAnalytics/Extensions/String+EXT.swift
@@ -7,17 +7,17 @@
 import Foundation
 
 extension String {
-    func clipped(maxCharacters: Int) -> String {
+    private func clipped(maxCharacters: Int) -> String {
         String(prefix(maxCharacters))
     }
     
-    func replaceSpacesWithUnderscores() -> String {
+    private func replaceSpacesWithUnderscores() -> String {
         self.replacingOccurrences(of: " ", with: "_")
     }
     
     func clean(maxCharacters: Int) -> String {
         self
-            .clipped(maxCharacters: 40)
+            .clipped(maxCharacters: maxCharacters)
             .replaceSpacesWithUnderscores()
     }
 }
